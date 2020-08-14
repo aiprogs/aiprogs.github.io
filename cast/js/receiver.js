4f70dfec6010a58af54096c854bc1fd10144fc3c
Igor Lezhnev <i.lezhnev@gmail.com>
2020-08-14 16:44:12 +0300

    Migrate to normal serv

--- cast/js/receiver.js
+++
@@ -1,7 +0,0 @@
-const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
-castDebugLogger.setEnabled(true);
-castDebugLogger.showDebugLogs(true);
-castDebugLogger.loggerLevelByEvents = {
-    'cast.framework.events.category.CORE': cast.framework.LoggerLevel.DEBUG,
-    'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
-}
