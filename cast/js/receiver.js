1b22082ad4f393930f32b6ccaacaa3cacded95e7
Igor Lezhnev <i.lezhnev@gmail.com>
2020-08-14 16:18:25 +0300

    Operations

--- cast/js/receiver.js
+++ cast/js/receiver.js
@@ -1,2 +1,8 @@
 const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
 castDebugLogger.setEnabled(true);
+castDebugLogger.showDebugLogs(true);
+castDebugLogger.loggerLevelByEvents = {
+    'cast.framework.events.category.CORE': cast.framework.LoggerLevel.DEBUG,
+    'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
+}
+console.log(cast.framework.CastReceiverContext.getInstance().getApplicationData().sessionId);
